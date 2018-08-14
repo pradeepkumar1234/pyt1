b7c32c574faecd28a8d251a4dc5d3a6be421fc9a
pradeepkumar1234 <42204097+pradeepkumar1234@users.noreply.github.com>
2018-08-14 09:50:30 +0530

    Create voworcon.py

---
+++ voworcon.py
@@ -0,0 +1,9 @@
+ch=input("")
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+	if(ch=='a',ch=='e',ch=='i',ch=='o',ch=='u',ch=='A',ch=='E',ch=='I',ch=='O',ch=='U'):
+		print("Vowel")
+	else:
+			print("Constant")
+else:
+	print("invalid")
+		
