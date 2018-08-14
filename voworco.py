b3e711279d702d2fd5644811657e99f1c2d613b1
pradeepkumar1234 <42204097+pradeepkumar1234@users.noreply.github.com>
2018-08-14 09:58:01 +0530

    Rename voworcon.py to voworco.py

---
+++ voworco.py
@@ -0,0 +1,9 @@
+ch=input("")
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+	if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+		print("Vowel",end="")
+	else:
+		print("Constant",end="")
+else:
+	print("invalid",end="")
+		
